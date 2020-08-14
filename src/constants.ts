d5f41542c706d249f08ef33179e55d38327ebbf2
ChrisTVH <cristoferherame@gmail.com>
2020-08-14 10:01:12 -0400

    Versión de Chrome Actualizada

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ import * as path from 'path';
 
 export const DEFAULT_APP_NAME = 'APP';
 
-// Update both together
+// Actualizar ambos juntos
 export const DEFAULT_ELECTRON_VERSION = '9.2.0';
-export const DEFAULT_CHROME_VERSION = '83.0.4103.122';
+export const DEFAULT_CHROME_VERSION = '84.0.4147.94';
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
   DEFAULT_ELECTRON_VERSION.split('.')[0],
