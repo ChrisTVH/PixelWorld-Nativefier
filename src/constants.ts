732f6ebf770919510418254200ea5453f1e6e4a3
Christopher Herasme <cristoferherame@gmail.com>
2020-08-28 09:11:13 -0400

    Pequeños cambios

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 export const DEFAULT_APP_NAME = 'APP';
 
 // Actualizar ambos juntos
-export const DEFAULT_ELECTRON_VERSION = '10.0.0';
+export const DEFAULT_ELECTRON_VERSION = '9.2.0';
 export const DEFAULT_CHROME_VERSION = '83.0.4103.122';
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
